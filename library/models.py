7f4fa9f18358b1b2187ad7d50493c08480a03932
Andriy Hrudnyy <andgrudniy@gmail.com>
2025-06-06 21:53:26 +0200

    Added models and registered them in admin panel

--- library/models.py
+++ library/models.py
@@ -1,3 +1,62 @@
+from decimal import Decimal
 from django.db import models
+from user.models import User
 
-# Create your models here.
+
+class Book(models.Model):
+    class CoverType(models.TextChoices):
+        HARD = "HARD", "Hard"
+        SOFT = "SOFT", "Soft"
+
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    cover = models.CharField(
+        max_length=4,
+        choices=CoverType.choices,
+        default=CoverType.SOFT,
+    )
+    inventory = models.PositiveIntegerField(default=0)
+    daily_fee = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.email} - {self.book.title}"
+
+
+class Payment(models.Model):
+    class Status(models.TextChoices):
+        PENDING = "PENDING", "Pending"
+        PAID = "PAID", "Paid"
+
+    status = models.CharField(
+        max_length=10,
+        choices=Status.choices,
+        default=Status.PENDING,
+    )
+    borrowing = models.OneToOneField(
+        Borrowing,
+        on_delete=models.CASCADE,
+        related_name="payment"
+    )
+    session_url = models.URLField(null=True, blank=True)
+    session_id = models.CharField(max_length=255, null=True, blank=True)
+    money_for_book = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        default=Decimal("0.00")
+    )
+    money_for_late = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        default=Decimal("0.00")
+    )
+
+    def __str__(self):
+        return f"Payment for Borrowing #{self.borrowing.id} - {self.status}"
