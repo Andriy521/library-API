f96431f58e42b2269c23da688ff368f3e6a30978
Andriy Hrudnyy <andgrudniy@gmail.com>
2025-06-08 19:33:23 +0200

    Added borrowing return function with aytomatic payment creation and also added payment readonly view

--- library/models.py
+++ library/models.py
@@ -3,11 +3,22 @@ from django.db import models
 from user.models import User
 
 
-class Book(models.Model):
-    class CoverType(models.TextChoices):
-        HARD = "HARD", "Hard"
-        SOFT = "SOFT", "Soft"
+class CoverType(models.TextChoices):
+    HARD = "HARD", "Hard"
+    SOFT = "SOFT", "Soft"
 
+
+class PaymentStatus(models.TextChoices):
+    PENDING = "PENDING", "Pending"
+    PAID = "PAID", "Paid"
+
+
+class PaymentType(models.TextChoices):
+    PAYMENT = "PAYMENT", "Payment"
+    FINE = "FINE", "Fine"
+
+
+class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     cover = models.CharField(
@@ -29,34 +40,38 @@ class Borrowing(models.Model):
     def __str__(self):
         return f"{self.user.email} - {self.book.title}"
 
+    def calculate_payment_amounts(self):
+        """Повертає кортеж (payment_amount, fine_amount)"""
+        days_borrowed = (self.expected_return_date - self.borrow_date).days
+        payment_amount = Decimal(days_borrowed * self.book.daily_fee)
+
+        fine_amount = Decimal(0)
+        if self.actual_return_date and self.actual_return_date > self.expected_return_date:
+            late_days = (self.actual_return_date - self.expected_return_date).days
+            fine_amount = Decimal(late_days * self.book.daily_fee * 2)
+
+        return payment_amount, fine_amount
+
+
 
 class Payment(models.Model):
-    class Status(models.TextChoices):
-        PENDING = "PENDING", "Pending"
-        PAID = "PAID", "Paid"
-
     status = models.CharField(
         max_length=10,
-        choices=Status.choices,
-        default=Status.PENDING,
+        choices=PaymentStatus.choices,
+        default=PaymentStatus.PENDING,
     )
-    borrowing = models.OneToOneField(
+    type = models.CharField(
+        max_length=10,
+        choices=PaymentType.choices
+    )
+    borrowing = models.ForeignKey(
         Borrowing,
         on_delete=models.CASCADE,
-        related_name="payment"
+        related_name="payments"
     )
     session_url = models.URLField(null=True, blank=True)
     session_id = models.CharField(max_length=255, null=True, blank=True)
-    money_for_book = models.DecimalField(
-        max_digits=6,
-        decimal_places=2,
-        default=Decimal("0.00")
-    )
-    money_for_late = models.DecimalField(
-        max_digits=6,
-        decimal_places=2,
-        default=Decimal("0.00")
-    )
+    money_to_pay = models.DecimalField(max_digits=8, decimal_places=2)
 
     def __str__(self):
         return f"Payment for Borrowing #{self.borrowing.id} - {self.status}"
