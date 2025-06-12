4107fe4dd2cb9c165d7dc88cf7f78f6337f788e8
Andriy Hrudnyy <andgrudniy@gmail.com>
2025-06-12 22:00:57 +0200

    Full migration to Docker, fixed URLs

--- user/urls.py
+++ user/urls.py
@@ -4,10 +4,10 @@ from .views import CustomTokenObtainPairView, MeView, RegisterView, api_root
 
 urlpatterns = [
     path("", api_root, name="api-root"),
-    path("users/", RegisterView.as_view(), name="register"),
-    path("users/token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("users/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("users/me/", MeView.as_view(), name="user_me"),
+    path("", RegisterView.as_view(), name="register"),
+    path("token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("me/", MeView.as_view(), name="user_me"),
 ]
 
 app_name = "users"
